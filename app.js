382aaa63a0b7f2da05f4e812d94f3ae804fd6f71
vigneshwaranr23 <vigneshwaranr23@gmail.com>
2017-06-16 15:29:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 
 var express=require('express');
 var app=express();
-var port=process.env.port;
+var ab=process.env.port;
 app.get('/',function(req,res){
     res.send("Hello World");
     res.end();
 });
-app.listen(port,function(){
-  console.log("App is running in port" +port);
+app.listen(ab,function(){
+  console.log("App is running in port" +ab);
 });
