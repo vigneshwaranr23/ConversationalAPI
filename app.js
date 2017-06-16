44eb3e5471f124dc2ddf0ed63d55a3a5bd09fc5d
vigneshwaranr23 <vigneshwaranr23@gmail.com>
2017-06-16 16:35:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-
-var express=require('express');
+var express=require('express'),
+var apiai=require('api.ai');
+var aiengine=apiai('19e8670c945d4a6a864d01c421f4a363');
 var app=express();
-var ab=process.env.PORT || 3000;
+var port=process.env.port||5001;
 app.get('/',function(req,res){
     res.send("Hello World");
     res.end();
 });
-app.listen(ab,function(){
-  console.log("App is running in port" +ab);
+app.post('/',function(req,res){
+   console.log(req);
+   })
+ });
+app.listen(port,function(){
+  console.log("App is running in port" +port);
 });
