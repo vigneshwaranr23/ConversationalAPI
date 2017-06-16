a5af6ce8e0ae41ebd9703a6957b3a3f499e39fcb
vigneshwaranr23 <vigneshwaranr23@gmail.com>
2017-06-16 14:57:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var http=require('http');
-http.createServer(function(req,res){
-  var port=process.env.port;
-res.writeHead(200,'Content-Type{"text/json"}');
-res.write("Welcome to First Bot Examples");
-res.end();
+var port=process.env.port;
+
+http.createServer(function(req,res){  
+  res.writeHead(200,'Content-Type{"text/json"}');
+  res.write("Welcome to First Bot Examples");
+  res.end();
 }).listen(port);
 console.log('server is running at port 5001')
