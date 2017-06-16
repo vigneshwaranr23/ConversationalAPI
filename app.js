182f9b82bd287b8f8102c4c26313f44222fd8ee8
vigneshwaranr23 <vigneshwaranr23@gmail.com>
2017-06-16 18:34:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express=require('express');
 var apiai=require('apiai');
 var aiengine=apiai('19e8670c945d4a6a864d01c421f4a363');
 var app=express();
-var port=process.env.port||5001;
+var port=process.env.PORT||5001;
 //app.get('/',function(req,res){
   //  res.send("Hello World");
     //res.end();
