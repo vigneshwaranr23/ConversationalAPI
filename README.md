7aaf13df0d87a8a059a59d12d6b86463e5a26677
vigneshwaranr23 <vigneshwaranr23@gmail.com>
2017-06-16 17:46:48 +0530

    modified app.js

--- README.md
+++
@@ -1,2 +0,0 @@
-# ConversationalAPI
-ConersationalAPI
--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-var http=require('http');
-http.createServer(function(req,res){
-res.writeHead(200,'Content-Type{"text/json"}');
-res.write("Welcome to First Bot Examples");
-res.end();
-}).listen(5001);
-console.log('server is running at port 5001')
+var express=require('express');
+var ai=require('apiai');
+var aiengine=apiai('19e8670c945d4a6a864d01c421f4a363');
+var app=express();
+var port=process.env.port||5001;
+//app.get('/',function(req,res){
+  //  res.send("Hello World");
+    //res.end();
+//});
+app.post('/',function(req,res){
+   console.log(JSON.stringify(req.body));
+ });
+app.listen(port,function(){
+  console.log("App is running in port" +port);
+});
