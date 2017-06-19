1bdf78c48e6de7b389faee1db30587f6c878bca5
vigneshwaranr23 <vigneshwaranr23@gmail.com>
2017-06-19 14:02:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 var express=require('express');
+
 var bodyParser = require('body-parser');
+
 var apiai=require('apiai');
+
 var aiengine=apiai('19e8670c945d4a6a864d01c421f4a363');
+
 var app=express();
+
 var port=process.env.PORT||5001;
+
 app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({ extended: true }));
-app.get('/',function(req,res){
-    res.send("Hello World");
-    res.end();
-});
+
 app.post ('/',function(req,res){
   var apiagentreq=req.body.result&&req.body.result.parameters;
   var resagent='hello welcome to TICKET BOOK chat bot';
