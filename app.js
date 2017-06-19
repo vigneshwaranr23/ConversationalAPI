b25c0826664623940a5b7cee8cfbc8cbea94bfe5
vigneshwaranr23 <vigneshwaranr23@gmail.com>
2017-06-19 13:04:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,16 +6,16 @@ var app=express();
 var port=process.env.PORT||5001;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-//app.get('/',function(req,res){
-  //  res.send("Hello World");
-    //res.end();
-//});
-app.post ('/',function(req,res){
-    req.on("data",function(obj){
-      console.log(obj);
-    });
-    console.log("HI test");
- });
+app.get('/',function(req,res){
+    res.send("Hello World");
+    res.end();
+});
+//app.post ('/',function(req,res){
+  //  req.on("data",function(obj){
+    //  console.log(obj);
+    // });
+    // console.log("HI test");
+ // });
 app.listen(port,function(){
   console.log("App is running in port" +port);
 });
