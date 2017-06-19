0c8575dfd5b37d8e39bdcb077693d1b875b1a103
vigneshwaranr23 <vigneshwaranr23@gmail.com>
2017-06-19 12:42:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 var express=require('express');
-var bodyParser=require('body-parser');
+var bodyParser = require('body-parser');
 var apiai=require('apiai');
 var aiengine=apiai('19e8670c945d4a6a864d01c421f4a363');
 var app=express();
 var port=process.env.PORT||5001;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/',function(req,res){
     res.send("Hello World");
     res.end();
 });
-app.post ('/',function(req,res){
+app.post ('/webhook',function(req,res){
     req.on("data",function(obj){
-      console.log(bodyParser.json(obj));
+      console.log(obj);
     });
     console.log("HI test");
  });
