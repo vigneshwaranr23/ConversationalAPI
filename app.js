28401fe478986d71ccc37fee74b37fae6bc050da
vigneshwaranr23 <vigneshwaranr23@gmail.com>
2017-06-21 15:52:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post ('/',function(req,res){
+  console.log(JSON.parse(req));
   var apiagentreq=req.body.result&&req.body.result.parameters;
   var resagent='Your Ticket has been blocked please choose you paymentType';
   console.log('request are'+apiagentreq);
